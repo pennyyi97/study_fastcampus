7ca0f8f27924a388601321a8bf94272dd7569a10
kyeong vin(penny) <pennyyi97@gmail.com>
2021-02-07 14:56:11 +0900

    21.02.07 배열 기초

---
+++ script_class/class13.js
@@ -0,0 +1,12 @@
+/* 배열 */
+
+const array = [1, 'lalalal', 3, 7];
+console.log(array[1]);
+
+const objects = [{ name: '멍멍이' }, { name: '야옹이' }, { name: '아슬란' }];
+
+objects.push({
+  name: '망뭉이'
+});
+
+console.log(objects.length);
